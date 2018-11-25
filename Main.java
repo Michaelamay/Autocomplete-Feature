e65fae4b5b9d4dd6236747e264635c40449be5a5
Michael Amay <michaelamay1@yahoo.com>
2018-11-24 22:05:56 -0600

    updated Main

--- Main.java
+++ Main.java
@@ -7,16 +7,19 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Wiktionary wik = new Wiktionary("wiktionary.txt");
-		System.out.println(wik.getSize());
-		
+		//System.out.println(wik.getSize());
+		System.out.println("Begin typing a word.");
 		while (true) {
 			Scanner console = new Scanner(System.in);
-			System.out.println("Type part of a word.  Type 'q' to stop");
+			//System.out.println();
+			//System.out.println("Type part of a word.  Type 'q' to stop");
+			//System.out.println("Begin typing a word.");
 			String prefix = console.next();
+			System.out.println();
 			if (prefix.equals("q")) return;
 			String[] best = wik.bestNMatches(prefix, 3);
 			for (String word : best)
-				System.out.print(word + " ");
+				System.out.println(word + " ");
 			System.out.println();
 		}
 	}
